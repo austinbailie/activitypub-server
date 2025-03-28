a213d782bcdc6414f91ba51a727fd2cf1770ded9
Austin Bailie <austinbailie@gmail.com>
2025-03-28 16:06:50 -0700

    we wont it work now

--- scripts/post.rb
+++ scripts/post.rb
@@ -6,22 +6,23 @@ require 'json'
 logger = Logger.new(STDOUT) # Log to console
 logger.level = Logger::DEBUG
 
-#document = File.read('./create-hello-world.json')
+document = File.read('./create-hello-world.json')
 
-document = {
-	"@context": "https://www.w3.org/ns/activitystreams",
-	"id": "https://activitypub-server-1040968594711.us-central1.run.app/create-hello-world/#{Time.now.utc.iso8601}",
-	"type": "Create",
-	"actor": "https://activitypub-server-1040968594711.us-central1.run.app/actors/earlyadopter",
-	"object": {
-		"id": "https://activitypub-server-1040968594711.us-central1.run.app/create-hello-world",
-		"type": "Note",
-		"published": "#{Time.now.utc.iso8601}",
-		"attributedTo": "https://activitypub-server-1040968594711.us-central1.run.app/actors/earlyadopter",
-		"content": "<p>Hello world from ActivityPub server</p>",
-		"to": "https://www.w3.org/ns/activitystreams#Public"
-	}
-}
+# document = {
+# 	"@context": "https://www.w3.org/ns/activitystreams",
+# 	"id": "https://activitypub-server-1040968594711.us-central1.run.app/create-hello-world/#{Time.now.utc.iso8601}",
+# 	"type": "Create",
+# 	"actor": "https://activitypub-server-1040968594711.us-central1.run.app/actors/earlyadopter",
+# 	"object": {
+# 		"id": "https://activitypub-server-1040968594711.us-central1.run.app/create-hello-world",
+# 		"type": "Note",
+# 		"published": "#{Time.now.utc.iso8601}",
+# 		"attributedTo": "https://activitypub-server-1040968594711.us-central1.run.app/actors/earlyadopter",
+# 		"content": "<p>Hello world from ActivityPub server SPECIAL</p>",
+# 		"to": "https://www.w3.org/ns/activitystreams#Public",
+#     "inReplyTo": "https://mastodon.social/@earlyadopter/1234567890"
+# 	}
+# }
 inbox_url = 'https://mastodon.social/inbox'
 actor_url = 'https://activitypub-server-1040968594711.us-central1.run.app/actors/earlyadopter'
 
@@ -29,7 +30,7 @@ logger.debug("HTTP POST Request to #{inbox_url}")
 logger.debug("Document being sent: #{document}")
 
 sha256        = OpenSSL::Digest::SHA256.new
-digest        = "SHA-256=" + Base64.strict_encode64(sha256.digest(document.to_json))
+digest        = "SHA-256=" + Base64.strict_encode64(sha256.digest(document))
 date          = Time.now.utc.httpdate
 keypair       = OpenSSL::PKey::RSA.new(File.read('private.pem'))
 signed_string = "(request-target): post /inbox\nhost: mastodon.social\ndate: #{date}\ndigest: #{digest}"
@@ -52,7 +53,7 @@ response = HTTP.headers({
   'Accept-Encoding': 'gzip, deflate, br',
   'Connection': 'keep-alive'
 })
-.post(inbox_url, body: document.to_json)
+.post(inbox_url, body: document)
 
 logger.debug("Response status: #{response.status}")
 logger.debug("Response headers: #{response.headers}")
